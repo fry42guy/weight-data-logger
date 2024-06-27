b82cdc7b7d0a873f46dfbba38f6901b0036c9c40
Fry42guy <Ryanlbaird@gmail.com>
2024-06-27 11:08:14 -0500

    del

--- VoltageRatio-simple/My Project/Application.Designer.vb
+++ VoltageRatio-simple/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.temperaturesensor_thermocouple_simple.Form1
+            Me.MainForm = Global.temperaturesensor_thermocouple_simple.DataloggerForm
         End Sub
     End Class
 End Namespace
